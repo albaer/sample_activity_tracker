16c98a5e9ebf278ec83fe6f52c716032db3c7e1d
Annie Baer <albaer@gmail.com>
2016-09-19 14:54:58 -0400

    Scaffold Activity model and related behavior.

---
+++ db/migrate/20160919175501_create_activities.rb
@@ -0,0 +1,11 @@
+class CreateActivities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.datetime :start_time
+      t.datetime :end_time
+
+      t.timestamps
+    end
+  end
+end
